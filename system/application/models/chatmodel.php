b185cc900bda9e081e692db1904ad656602fdf18
Logan Lindquist <logan@llbbl.com>
2010-09-07 20:30:24 -0500

    chat reworked

---
+++ system/application/models/chatmodel.php
@@ -0,0 +1,24 @@
+<?php
+
+
+class chatmodel extends Model
+{
+	function __construct()
+	{
+		parent::Model();
+	}
+	
+	
+	function getMsg($limit = 10)
+	{
+		$sql = "SELECT * FROM messages ORDER BY id DESC LIMIT $limit";		
+		return $this->db->query($sql);
+	}
+	
+	function insertMsg($name, $message, $current)
+	{
+		$sql = "INSERT INTO messages SET user='$name', msg='$message', time='$current'";
+		return $this->db->query($sql);
+	}
+}
+
