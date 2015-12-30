5a809cc91cf7be8f75d7a2c2eea7447b49eb4bd2
Logan Lindquist <logan@llbbl.com>
2015-12-30 11:54:16 -0600

    upgrading to codeigniter 3.0
    all the core and system files

--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -1,4 +1,5 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Welcome extends CI_Controller {
 
@@ -7,10 +8,10 @@ class Welcome extends CI_Controller {
 	 *
 	 * Maps to the following URL
 	 * 		http://example.com/index.php/welcome
-	 *	- or -  
+	 *	- or -
 	 * 		http://example.com/index.php/welcome/index
 	 *	- or -
-	 * Since this controller is set as the default controller in 
+	 * Since this controller is set as the default controller in
 	 * config/routes.php, it's displayed at http://example.com/
 	 *
 	 * So any other public methods not prefixed with an underscore will
@@ -22,6 +23,3 @@ class Welcome extends CI_Controller {
 		$this->load->view('welcome_message');
 	}
 }
-
-/* End of file welcome.php */
-/* Location: ./application/controllers/welcome.php */
