b522619fb597595dcf14ee5cd28626b76ce6f65b
Logan Lindquist <logan@llbbl.com>
2015-12-30 21:08:29 -0600

    updating default route and forcing post route

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,6 +49,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['default_controller'] = 'welcome';
+$route['default_controller'] = 'chat';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
+
+$route['chat/update']['post'] = 'chat/update';
+$route['chat/update']['get'] = 'chat/index';
+
