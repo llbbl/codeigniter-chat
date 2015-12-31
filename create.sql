68a40c4d5fb745b1a3a971a0989ab7801cbe6507
Logan Lindquist <logan@llbbl.com>
2015-12-30 21:22:45 -0600

    new table structure

--- create.sql
+++ create.sql
@@ -1,45 +1,30 @@
--- Generation Time: Jul 22, 2013 at 02:56 PM
--- Server version: 5.1.53
--- PHP Version: 5.3.13
-
-CREATE SCHEMA cichat;
-USE cichat;
-
-
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+#
+# SQL Export
+# Created by Querious (1014)
+# Created: December 30, 2015 at 9:16:39 PM CST
+# Encoding: Unicode (UTF-8)
+#
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+
+CREATE TABLE `ci_sessions` (
+  `id` varchar(40) NOT NULL,
+  `ip_address` varchar(45) NOT NULL,
+  `timestamp` int(10) unsigned NOT NULL DEFAULT '0',
+  `data` blob NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `ci_sessions_timestamp` (`timestamp`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
--- --------------------------------------------------------
-
---
--- Table structure for table `ci_sessions`
---
-
-CREATE TABLE IF NOT EXISTS `ci_sessions` (
-  `session_id` VARCHAR(40) NOT NULL DEFAULT '0',
-  `ip_address` VARCHAR(16) NOT NULL DEFAULT '0',
-  `user_agent` VARCHAR(50) NOT NULL,
-  `last_activity` INT(10) UNSIGNED NOT NULL DEFAULT '0',
-  `session_data` TEXT,
-  PRIMARY KEY (`session_id`)
-) ENGINE=MYISAM DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `messages`
---
-
-CREATE TABLE IF NOT EXISTS `messages` (
-  `id` INT(7) NOT NULL AUTO_INCREMENT,
-  `user` VARCHAR(255) NOT NULL,
-  `msg` TEXT NOT NULL,
-  `time` INT(9) NOT NULL,
+CREATE TABLE `messages` (
+  `id` int(7) NOT NULL AUTO_INCREMENT,
+  `user` varchar(255) CHARACTER SET latin1 NOT NULL,
+  `msg` text CHARACTER SET latin1 NOT NULL,
+  `time` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=MYISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+
+
+
+
